c671f41ee125d911f11e8018f65376c0dbe65df0
13krsnaa <saxenakrishna9140@gmail.com>
2026-04-01 17:42:54 +0530

    basic middleware

--- middlewares/server.js
+++ middlewares/server.js
@@ -2,6 +2,26 @@ const express = require("express");
 
 const app = express();
 
+//* Basic middleware *//
+
+app.use((req, res) => {
+  let { query } = res.query;
+  console.log("This is a middleware");
+  res.send(
+    "I am a middleware and none of your routes will work in fornt of me ",
+  );
+  console.log(query);
+});
+
+//* basic requests *//
+app.get("/", (req, res) => {
+  res.send("Hii, i am homepage");
+});
+
+app.get("/random", (req, res) => {
+  res.send("this is a random page");
+});
+
 app.listen(3000, () => {
   console.log("Server is runing on port 3000");
 });
